de80188b0ee9be91033173c2ae512a5b03026c2d
jennianelson <jennifernelson118@gmail.com>
2018-05-03 16:17:59 -0500

    Added modules directory and created HomeModule

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import {Container} from './components/primitives';
+import {Container, MainContainer} from './components/primitives';
 import {Sidebar, Header} from './components/ui';
+
+import HomeModule from './components/modules/HomeModule';
 // import logo from './logo.svg';
 import './styles/style.css';
 
@@ -11,19 +13,9 @@ class App extends Component {
         <Sidebar className="aside collapse" id="collapseExample"></Sidebar>
 				<Container className="main-container">
 						<Header className="header"></Header>
-
-						<main className="main">
-								<div className="main-content">
-										<div className="desktop-platform">
-												<img src={require('./assets/images/dual-monitor-desktop-platform.png')} alt="" className="img-fluid"/>
-										</div>
-										<hgroup>
-												<h1>Don't let profitable moments pass you by...</h1>
-												<h2>Real-time mortgage data, advanced analytics, and reporting at your fingertips</h2>
-										</hgroup>
-										<div className="btn btn-rounded btn-primary lg mb-4">Get Started</div>
-								</div>
-						</main>
+						<MainContainer>
+							<HomeModule></HomeModule>
+						</MainContainer>
 				</Container>
     	</Container>
     );
