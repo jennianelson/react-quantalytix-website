80596a57d76783b7a6256c3d6f075f3a146c17aa
jennianelson <jennifernelson118@gmail.com>
2018-05-10 09:11:37 -0500

    Added Scss loader to webpack config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import HomeScreen from './components/screens/HomeScreen';
 import AboutUsScreen from './components/screens/AboutUsScreen';
 // import logo from './logo.svg';
-import './styles/style.css';
+import './styles/style.scss';
 
 class App extends Component {
   render() {
