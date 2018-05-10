c7cdfa411215fe529b216adb98bffe3e84a19e9b
jennianelson <jennifernelson118@gmail.com>
2018-05-10 15:51:37 -0500

    Moved Image component to primitives

---
+++ src/components/primitives/Image.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((item, index) => { 
+    images[item.replace('./', '')] = r(item); 
+  });
+  return images;
+}
+
+const images = importAll(require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/));
+
+const Image = props => {
+  return (
+    <div className={props.className}>
+      <img src={images[props.imageName]} alt="" className="img-fluid"/>
+    </div>
+  )
+}
+
+export default Image;
