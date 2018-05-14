3e5cbcd34b42c361a8895348052232c5e30ba2f8
jennianelson <jennifernelson118@gmail.com>
2018-05-14 17:06:56 -0500

    Created FeaturesList part for MarketDataPricing

--- src/components/parts.jsx
+++ src/components/parts.jsx
@@ -3,11 +3,13 @@ import UsernameField from './parts/UsernameField';
 import EmailField from './parts/EmailField';
 import PhoneField from './parts/PhoneField';
 import CompanyField from './parts/CompanyField';
+import FeaturesList from './parts/FeaturesList';
 
 export {
   InputGroup,
   UsernameField,
   EmailField,
   PhoneField,
-  CompanyField
+  CompanyField,
+  FeaturesList
 }
---
+++ src/components/parts/FeaturesList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Image} from '../primitives';
+
+const FeaturesList = ({list}) => {
+  return (
+    <ul className="pricing-fetures">
+      {list.map((item, index) => {
+        return (
+          <li key={index}>
+            <span className="checked-icon"><Image imageName="checked-price.svg"/></span>
+            <span>{item}</span>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default FeaturesList;
