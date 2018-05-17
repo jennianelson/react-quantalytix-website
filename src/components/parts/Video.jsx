c5a754753be9ab9370bf270b81d136c38d467a4a
jennianelson <jennifernelson118@gmail.com>
2018-05-17 11:21:06 -0500

    Installed react-player and got video rendering and responsive

--- src/components/parts/Video.jsx
+++ src/components/parts/Video.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-
-import styled from 'styled-components';
-// import "~video-react/dist/video-react.css";
-
-// const videoStyle = styled.button`
-//   background: url("../../assets/images/video-poster.png")
-// `
+import ReactPlayer from 'react-player';
 
 const Video = () => {
   return (
-    <div className="html5-video-container" data-layer="0">
-      {/* // <Player>
-      // <source src="https://www.youtube.com/embed/hgzzLIa-93c?autoplay=1&modestbranding=1&rel=0&hl=sv"/>
-      // <button className="videoPoster js-videoPoster" style={videoStyle}>Play video</button>
-      // </Player> */}
+    <div className='videoWrapper videoWrapper169 js-videoWrapper'>
+        <ReactPlayer
+          className='videoIframe'
+          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+          width='100%'
+          height='100%'
+        />
     </div>
   )
 }
