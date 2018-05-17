124cbdc9d59321dc24bf148e70cc73edb5584b4e
jennianelson <jennifernelson118@gmail.com>
2018-05-17 08:41:44 -0500

    Cleaned up/Deleted unused code

--- src/components/parts/Video.jsx
+++ src/components/parts/Video.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const videoBG = {
-  background: require('../../assets/images/video-poster.png')
-}
+// const videoBG = {
+//   background: require('../../assets/images/video-poster.png')
+// }
 
 const Video = () => {
   return (
