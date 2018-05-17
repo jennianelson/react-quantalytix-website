d28cd0cedc30070b2d1738562e90676d2257b694
jennianelson <jennifernelson118@gmail.com>
2018-05-17 08:43:13 -0500

    adjusted format of code in AboutUsScreen

--- src/components/sections/AboutUs/AboutUsScreen.jsx
+++ src/components/sections/AboutUs/AboutUsScreen.jsx
@@ -8,19 +8,17 @@ export default class AboutUsScreen extends Component {
   render() {
     return (
       <PageTemplate pageWrapper="about-page">
-        
-            <MainContent className="mt-md-5">
-              <Image className="about-logo" imageName="about-logo.svg" />
-              <hgroup className="py-4">
-                <h1>OUR STORY</h1>
-              </hgroup>
-              <div className="col-md-8 offset-md-2">
-                <p>Quantalytix is a software platform that provides comprehensive risk analytics and balance sheet management for mortgage lenders. We deliver seamless, agile, analytics that give our customers an unfair advantage over the competition.</p>
-                <p>We love data and analytics and we want to bring advanced analytics to everyone. We believe that data and analytics are useless if actionable decisions cannot be made from it. Therefore, we are on a mission to simplify and visualize key performance and risk metrics to help make your organization more proﬁtable.</p>
-                <Button kind="primary" size="lg" margins="mt-md-5 mb-4">Get Started</Button>
-              </div>
-            </MainContent>
-       
+        <MainContent className="mt-md-5">
+          <Image className="about-logo" imageName="about-logo.svg" />
+          <hgroup className="py-4">
+            <h1>OUR STORY</h1>
+          </hgroup>
+          <div className="col-md-8 offset-md-2">
+            <p>Quantalytix is a software platform that provides comprehensive risk analytics and balance sheet management for mortgage lenders. We deliver seamless, agile, analytics that give our customers an unfair advantage over the competition.</p>
+            <p>We love data and analytics and we want to bring advanced analytics to everyone. We believe that data and analytics are useless if actionable decisions cannot be made from it. Therefore, we are on a mission to simplify and visualize key performance and risk metrics to help make your organization more proﬁtable.</p>
+            <Button kind="primary" size="lg" margins="mt-md-5 mb-4">Get Started</Button>
+          </div>
+        </MainContent>
     	</PageTemplate>
     )
   }
