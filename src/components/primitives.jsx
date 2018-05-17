31d0ddede7a35143d6c688a4e71edbb3264f2578
jennianelson <jennifernelson118@gmail.com>
2018-05-17 09:45:26 -0500

    Created Row and Col primitives

--- src/components/primitives.jsx
+++ src/components/primitives.jsx
@@ -1,9 +1,13 @@
 import Button from './primitives/Button';
 import Image from './primitives/Image';
 import TextField from './primitives/TextField';
+import Row from './primitives/Row';
+import Col from './primitives/Col';
 
 export {
   Button,
   Image,
-  TextField
+  TextField,
+  Row,
+  Col
 };
---
+++ src/components/primitives/Col.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Col = ({className, children}) => (<div className={className}>{children}</div>)
+
+export default Col;
