04d9ce6768efdf6c81afcb3033ca3788fd925e2a
jennianelson <jennifernelson118@gmail.com>
2018-05-22 16:13:24 -0500

    Added key to rendering of FeatureListItem in FeatureList component

--- src/components/sections/Products & Services/FeaturesList.jsx
+++ src/components/sections/Products & Services/FeaturesList.jsx
@@ -4,9 +4,10 @@ import FeaturesListItem from './FeaturesListItem';
 const FeaturesList = ({className, featuresArray}) => {
   return (
     <ul className={className}>
-      {featuresArray.map(featureObj => {
+      {featuresArray.map((featureObj, i) => {
         return (
         <FeaturesListItem
+          key={i}
           image={featureObj.image}
           heading={featureObj.heading}
           description={featureObj.description}
