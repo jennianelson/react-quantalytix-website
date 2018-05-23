44c1ca005baa5f256e4061487ca94318d2e2fc50
jennianelson <jennifernelson118@gmail.com>
2018-05-23 15:10:33 -0500

    Changed props structure of SocialMediaLink component

--- src/components/parts/SocialMediaLink.jsx
+++ src/components/parts/SocialMediaLink.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 
-const SocialMediaLink = ({url, iconName}) => {
+const SocialMediaLink = ({company}) => {
+  let url
+  let iconName
+  switch(company) {
+    case "twitter":
+      url = "https://twitter.com/quantalytix"
+      iconName="icon-tw"
+      break;
+    case "instagram":
+      url = "https://www.instagram.com/quantalytix/"
+      iconName="icon-ig"
+      break;
+    case "facebook":
+      url = ""
+      iconName="icon-fb"
+      break;
+    default:
+      url = "https://www.instagram.com/quantalytix/"
+      iconName="icon-ln";
+  }
+
   return (
     <li className="socials-item"><a href={url} target="blank"><i className={iconName}></i></a></li>
   )
--- src/components/ui/SocialMediaBar.jsx
+++ src/components/ui/SocialMediaBar.jsx
@@ -4,10 +4,10 @@ import {SocialMediaLink} from '../parts';
 const SocialMediaBar = () => {
   return (
     <ul className="socials">
-      <SocialMediaLink url="https://twitter.com/quantalytix" iconName="icon-tw"/>
-      {/* <SocialMediaLink url="" iconName="icon-fb"/> */}
-      <SocialMediaLink url="https://www.linkedin.com/company/quantalytix/" iconName="icon-ln"/>
-      <SocialMediaLink url="https://www.instagram.com/quantalytix/" iconName="icon-ig"/>
+      <SocialMediaLink company="linkedin"/>
+      {/* <SocialMediaLink company="facebook"/> */}
+      <SocialMediaLink company="instagram"/>
+      <SocialMediaLink company="twitter"/>
     </ul>
   )
 }
