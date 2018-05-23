33f527c21abef69892775c753093eaabe2999a1d
jennianelson <jennifernelson118@gmail.com>
2018-05-23 08:47:51 -0500

    index.js was importing a default stylesheet that was interferring with body style

--- src/components/sections/Products & Services/MarketDataScreen.jsx
+++ src/components/sections/Products & Services/MarketDataScreen.jsx
@@ -12,26 +12,25 @@ export default class MarketDataScreen extends Component {
     return (
       <PageTemplate className="market-data-page">
         <MainContent>
+        <h1 className="mb-4">MARKET DATA</h1>
+        <h2>Informed Decision-making <span className="circle-accent">&#9679;</span> Uncompromising Accessibility</h2>
           <InternalContent className="container px-xl-5 pt-4">
             <Row className="row">
-              <Col className="col-md-6 text-left pr-xl-5">
-                <h3 className="mb-4">MARKET DATA</h3>
-                {/* <h4 className="text-center">Informed Decision-making, Uncompromising Accessibility</h4> */}
-                  <p>We deliver data through an intuitive platform that maximizes collaboration and limits bottlenecks. Users not only choose the data they need but also benefit from enterprise-focused licensing.</p>
-                  <p>Whether you're looking for a custom data solution or to replace your current expensive terminal, we are your platform. We deliver all the core functionality you need like multi-asset class coverage, real-time news, economic events, and excel download.</p>
-                  <p>If you're ready to align your organizations goals with actionable data, contact us now to see how you can get started.</p>
-                 
-                  <FeaturesList className="market-data-list" featuresArray={marketDataFeatures}/>
+              <Col className="col-md-12 text-left pr-xl-5">
+                <p>We deliver data through an intuitive platform that maximizes collaboration and limits bottlenecks. Users not only choose the data they need but also benefit from enterprise-focused licensing.</p>
+                <p>Whether you're looking for a custom data solution or to replace your current expensive terminal, we are your platform. We deliver all the core functionality you need like multi-asset class coverage, real-time news, economic events, and excel download.</p>
+                <p>If you're ready to align your organizations goals with actionable data, contact us now to see how you can get started.</p>
               </Col>
+            </Row>
+            <Row className="row">
               <Col className="col-md-6">
+                <FeaturesList className="market-data-list" featuresArray={marketDataFeatures}/>
+              </Col>
+              <Col className="col-md-6 mt-4">
                 <Image imageName="market-data-device-platform.png"/>
-                <Col className="col-12 py-4">
-                  <Video />
-                </Col>
                 <Button kind="primary" margins="md mt-3">Request demo</Button>
               </Col>
             </Row>
-            {/* <PricingSection /> */}
           </InternalContent>
         </MainContent>
       </PageTemplate>
