01a44725c9e14629964a94681f94311e511dbfd0
jennianelson <jennifernelson118@gmail.com>
2018-05-30 16:48:34 -0500

    Removed ContactForm from ContactScreen

--- src/components/sections/ContactUs/ContactUsScreen.jsx
+++ src/components/sections/ContactUs/ContactUsScreen.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PageTemplate from '../../widgets/PageTemplate';
 import {MainContent, InternalContent} from '../../Wrappers';
-import {Col} from '../../primitives';
+import {Col, Image} from '../../primitives';
 import ContactUsForm from './ContactUsForm';
 import QContactsBlock from './QContactsBlock';
 
@@ -10,16 +10,25 @@ export default class ContactUsScreen extends Component {
     return ( 
     <PageTemplate className="about-page">
       <MainContent className="mt-5 ml-5">
-        <InternalContent className="d-md-flex">
-          <Col className="col-md-6 px-md-0">
+      <Col className="col-md-9">
+              <Image className="about-logo" imageName="about-logo.svg" />
+              <hgroup className="py-4">
+                <h1>Contact Us</h1>
+              </hgroup>
+     
+        <QContactsBlock/>
+      </Col>
+  
+        {/* <InternalContent className="d-md-flex"> */}
+          {/* <Col className="col-md-6 px-md-0">
             <div className="contact-form">
               <h1 className="mb-1"><span className="center-inner-div">CONTACT US</span></h1>
               <h2 className="mb-4">OUTMANEUVER. OUTSMART. OUTPERFORM.</h2>
               <ContactUsForm />
             </div>
-          </Col>
-          <QContactsBlock />
-        </InternalContent>
+          </Col> */}
+
+        {/* </InternalContent> */}
       </MainContent>
     </PageTemplate>
     )
