c1b2c49f6447c1bb4999a3c23b612e805ceb4977
jennianelson <jennifernelson118@gmail.com>
2018-05-31 10:04:36 -0500

    installed react-appinsights

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const appInsights = require('react-appinsights');
+appInsights.init({instrumentationKey:'428296fc-cd90-4203-8471-8665ad10c9de'});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
