ca8c47835f05fed37ec3a620484db93cc8e9144d
jennianelson <jennifernelson118@gmail.com>
2018-08-16 07:49:32 -0500

    ContactUsForm: Fixed error in handleChange() setState(); added async createContact method

--- client/src/components/sections/ContactUs/ContactUsForm.jsx
+++ client/src/components/sections/ContactUs/ContactUsForm.jsx
@@ -6,41 +6,63 @@ export default class ContactUsForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      contactInfo: { firstName: '',
-      lastName: '',
-      email: '',
-      company: '' },
-      submitted: false
+      contact: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        company: '' 
+      },
+      submitted: false,
+      error: false
     };
   }
 
   handleChange = (event) => {
+    // debugger
     const target = event.target;
     const value = target.value;
     const name = target.name;
     this.setState({
-      contactInfo: {
+      contact: {
+        ...this.state.contact,
         [name]: value
       }
     });
   }
 
+  async createContact(contact) {
+    let response = await fetch('/api/contacts', {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      credentials: 'include',
+      body: JSON.stringify(contact),
+    })
+    try {
+      let contact = await response.json()
+      return contact
+    } catch (error) {
+      console.log(error)
+      return { error: error }
+    } 
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    const data = this.state.contactInfo
-    fetch('/contacts', {
-      body: JSON.stringify(data),
-      headers: {'content-type': 'application/json'},
-      method: 'POST'
-      }).then(res => res.json())
-      .then(json => console.log(json));
-    
+    const contact = this.state.contact
+    let result = this.createContact(contact)
+
     this.setState({
-      contactInfo: { firstName: '',
-      lastName: '',
-      email: '',
-      company: '' },
-      submitted: true
+      contact: { 
+        firstName: '',
+        lastName: '',
+        email: '',
+        company: '' 
+      },
+      submitted: true,
+      contact: result,
+      error: result.error ? result.error : false
     });
   }
 
@@ -51,15 +73,16 @@ export default class ContactUsForm extends Component {
   }
 
   render() {
+    const {firstName, lastName, company, email} = this.state.contact
     return (
       <div>
          {this.state.submitted ? <Alert handleClick={(event) => this.closeAlert(event)} type="success">Thanks! You'll be hearing from us soon.</Alert> : null}
       <form onSubmit={(event) => this.handleSubmit(event)}>
-        <InputGroup handleChange={this.handleChange} name="firstName" value={this.state.contactInfo.firstName} placeholder="First Name" icon="user.svg" />
-        <InputGroup handleChange={this.handleChange} name="lastName" value={this.state.contactInfo.lastName} placeholder="Last Name" icon="user.svg" />
-        <InputGroup type="email" handleChange={this.handleChange} name="email" value={this.state.contactInfo.email} placeholder="Email" icon="email.svg" />
-        {/* <InputGroup handleChange={this.handleChange} name="phoneNumber" value={this.state.contactInfo.phoneNumber} placeholder="Phone Number" icon="phone.svg" /> */}
-        <InputGroup handleChange={this.handleChange} name="company" value={this.state.contactInfo.company} placeholder="Company" icon="company.svg" />
+        <InputGroup handleChange={this.handleChange} name="firstName" value={firstName} placeholder="First Name" icon="user.svg" />
+        <InputGroup handleChange={this.handleChange} name="lastName" value={lastName} placeholder="Last Name" icon="user.svg" />
+        <InputGroup type="email" handleChange={this.handleChange} name="email" value={email} placeholder="Email" icon="email.svg" />
+        {/* <InputGroup handleChange={this.handleChange} name="phoneNumber" value={phoneNumber} placeholder="Phone Number" icon="phone.svg" /> */}
+        <InputGroup handleChange={this.handleChange} name="company" value={company} placeholder="Company" icon="company.svg" />
         <Button type="submit" kind="primary" size="lg" margins="w-100 mt-5">CONTACT US!</Button>
       </form>
       </div>
